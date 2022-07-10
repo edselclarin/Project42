8fbd06ee17b37d4aedc832d654777708952a630d
Edsel Clarin <edsel.clarin@yahoo.com>
2022-07-09 23:12:54 -0700

    Add project files.

---
+++ Project42/Extension/StringExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace Project42.Extension
+{
+    public static class StringExtension
+    {
+        public static string ToCSV(this string? inputText)
+        {
+            if (String.IsNullOrEmpty(inputText))
+            {
+                return String.Empty;
+            }
+            else
+            {
+                var separators = new List<char>();
+                if (inputText.Contains('\r') || inputText.Contains('\n'))
+                {
+                    separators.Add('\r');
+                    separators.Add('\n');
+                }
+                else if (inputText.Contains('\t'))
+                {
+                    separators.Add('\t');
+                }
+                else
+                {
+                    separators.Add(' ');
+                }
+
+                var items = inputText.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                return String.Join(",", items);
+            }
+        }
+    }
+}
---
+++ Project42/Models/TextData.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project42.Models
+{
+    public class TextData
+    {
+        public string? InputText { get; set; }
+        public string? OutputText { get; set; }
+    }
+}
