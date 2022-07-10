110f5e9898a35d2d46ca50fc15dac9513627014c
Edsel Clarin <edsel.clarin@yahoo.com>
2022-07-09 23:34:11 -0700

    Automatically copy output text to clipboard.

--- Project42/Program.cs
+++ Project42/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Project42.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ClipboardService>();
 
 var app = builder.Build();
 
